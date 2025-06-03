f45814df847ac5f7842b72e6296ce0f8174bc413
Your Name <your.email@example.com>
2025-06-03 16:44:34 +0300

    add custom app bar to home view

---
+++ lib/Features/home/presentation/views/widgets/custom_app_bar.dart
@@ -0,0 +1,30 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 40),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Image.asset(
+            AssetsData.logo,
+            height: 20,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              CupertinoIcons.search,
+              size: 26,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
