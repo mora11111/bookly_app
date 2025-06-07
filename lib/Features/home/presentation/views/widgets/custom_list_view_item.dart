97066117b7828e52a177566b28dd57fe0a601f24
Your Name <your.email@example.com>
2025-06-08 01:27:40 +0300

    add custom list view item to book details view

--- lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,23 +1,30 @@
+import 'package:bookly_app/core/utils/app_router.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class FeaturedListViewItem extends StatelessWidget {
   const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.white,
-            image: const DecorationImage(
-              fit: BoxFit.fill,
-              image: AssetImage(
-                AssetsData.testImage,
-              ),
-            )),
+    return GestureDetector(
+      onTap: () {
+        GoRouter.of(context).push(AppRouter.kBookDetailsView);
+      },
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.white,
+              image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AssetsData.testImage,
+                ),
+              )),
+        ),
       ),
     );
   }
