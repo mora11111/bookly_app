055c02376852e185f0813f16613fe9fc19780e06
Your Name <your.email@example.com>
2025-06-08 21:14:55 +0300

    add rating to book details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly_app/core/utils/styles.dart';
@@ -29,9 +30,9 @@ class BookDetailsViewBody extends StatelessWidget {
               fontWeight: FontWeight.bold
             ),
           ),
-          const SizedBox(
-            height: 6,
-          ),
+          // const SizedBox(
+          //   height: 2,
+          // ),
           Opacity(
             opacity: .7,
             child: Text(
@@ -41,6 +42,12 @@ class BookDetailsViewBody extends StatelessWidget {
                 fontWeight: FontWeight.w500,
               ),
             ),
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          BookRating(
+            mainAxisAlignment: MainAxisAlignment.center,
           ),
         ],
       ),
--- lib/Features/home/presentation/views/widgets/book_rating.dart
+++ lib/Features/home/presentation/views/widgets/book_rating.dart
@@ -2,19 +2,20 @@ import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class BookRating extends StatelessWidget {
-  const BookRating({super.key});
+  const BookRating({super.key, this.mainAxisAlignment = MainAxisAlignment.end});
+  final MainAxisAlignment mainAxisAlignment;
 
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
+      mainAxisAlignment: mainAxisAlignment,
       children: [
         const Icon(
           Icons.star,
           color: Color(0xffFFDD4F),
         ),
         const SizedBox(
-          width: 6.3,
+          width: 5,
         ),
         const Text(
           '4.8',
