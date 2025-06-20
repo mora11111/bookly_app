017583d9b389902a2f89cdd5bf2f05ee0b2e5f8a
Your Name <your.email@example.com>
2025-06-20 23:00:41 +0300

    create server failure from dio error

--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -16,12 +17,15 @@ class HomeRepoImpl implements HomeRepo {
           endPoint:
               'volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest');
       List<BookModel> books = [];
-      for(var item in data['items']){
+      for (var item in data['items']) {
         books.add(BookModel.fromJson(item));
       }
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
--- lib/core/errors/failures.dart
+++ lib/core/errors/failures.dart
@@ -1,3 +1,33 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  const Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+
+  factory ServerFailure.fromDioError(DioException dioError) {
+    switch (dioError.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('Connection timeout with API server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('Send timeout with API server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('Receive timeout with API server');
+      case DioExceptionType.badCertificate:
+        return ServerFailure('Bad certificate from API server');
+      case DioExceptionType.badResponse:
+        return ServerFailure('Received invalid status code: ${dioError.response?.statusCode}');
+      case DioExceptionType.cancel:
+        return ServerFailure('Request to API server was cancelled');
+      case DioExceptionType.connectionError:
+        return ServerFailure('Connection to API server failed due to internet connection');
+      case DioExceptionType.unknown:
+      default:
+        return ServerFailure('Unexpected error occurred');
+    }
+  }
+}
