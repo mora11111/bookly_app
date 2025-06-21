8c5a55b3594c04d64444fbfe76db93ec10474148
Your Name <your.email@example.com>
2025-06-21 22:26:32 +0300

    display featured books part 1

--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -1,21 +1,39 @@
+import 'package:bookly_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/Features/home/presentation/views/widgets/custom_list_view_item.dart';
+import 'package:bookly_app/core/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * .3,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8.0),
-              child: FeaturedListViewItem(),
-            );
-          }),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery
+                .of(context)
+                .size
+                .height * .3,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: FeaturedListViewItem(),
+                  );
+                }),
+          );
+        } else if(state is FeaturedBooksFailure) {
+          return CustomErrorWidget(errMessage: state.errMessage);
+        }
+        else {
+          return const CustomLoadingIndicator();
+        }
+      },
     );
   }
 }
