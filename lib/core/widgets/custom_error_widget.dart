c004a2eaece3a317ae8d6ca191ffab7cceee32f7
Your Name <your.email@example.com>
2025-06-22 23:43:50 +0300

    fixing issue with loading data

--- lib/core/widgets/custom_error_widget.dart
+++ lib/core/widgets/custom_error_widget.dart
@@ -3,11 +3,16 @@ import 'package:flutter/material.dart';
 
 class CustomErrorWidget extends StatelessWidget {
   const CustomErrorWidget({super.key, required this.errMessage});
-  
+
   final String errMessage;
 
   @override
   Widget build(BuildContext context) {
-    return Text(errMessage , style: Styles.textStyle18,);
+    return Center(
+      child: Text(
+        errMessage,
+        style: Styles.textStyle18,
+      ),
+    );
   }
 }
